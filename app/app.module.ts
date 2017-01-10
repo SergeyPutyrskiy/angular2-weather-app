59f34efe66930fa28bce28ba851ebdfe9d577c2f
Sergey Putyrskiy <sputyrskiy@connecture.com>
2017-01-10 13:57:49 +0200

    added new module shared and some basic styles

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* Custom modules */
+import { SharedModule } from "./shared/shared.module";
+
+/* Custom components */
 import { AppComponent }  from './app.component';
+import { HomeComponent }  from './home.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, SharedModule ],
+  declarations: [ AppComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
