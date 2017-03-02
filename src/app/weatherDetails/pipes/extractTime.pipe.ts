db2a7667fa8242de5134b92a758ac17fe163b805
Sergey Putyrskiy <sputyrskiy@connecture.com>
2017-03-02 18:21:53 +0200

    Refactored custom pipe

--- src/app/weatherDetails/pipes/extractTime.pipe.ts
+++ src/app/weatherDetails/pipes/extractTime.pipe.ts
@@ -2,8 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'extractTime'})
 export class ExtractTime implements PipeTransform {
-  transform(value: String): String {
-    let startIndex, currentTime;
+  transform(value: string): string {
+    let startIndex: number, currentTime: string;
 
     if(value) {
       startIndex = value.search(/\s/);
